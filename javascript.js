2d3c9aa310f60830350387238f48cae94937c8ad
Jo Samuelsson <distress3@gmail.com>
2022-03-10 17:02:01 +0100

    Added functions for computer move and getting random number

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 //hello
+
+//create function computerPlay
+function computerPlay {
+    //create array "possibleMoves" with rock, paper, scissors
+    let possibleMoves = ['rock', 'paper', 'scissors'];
+    //create variable "randomNumber" with random number between 0 and 2
+    let randomNumber = getRandNum(2,0);
+    //create variable computerChoice with the string at position(randomNumber)
+    let computerChoice = possibleMoves[randomNumber]
+    //return computerChoice
+    return computerChoice;
+}
+
+//create function getRandNum with parameters for max and min values
+function getRandNum (max, min) {
+    //return a random integer between max and min parameters
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+//print the returned value from computerPlay
+console.log(computerPlay);
