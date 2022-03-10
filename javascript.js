12539f12daa40035fd327c7053aada46cef0a50c
Jo Samuelsson <distress3@gmail.com>
2022-03-10 17:37:14 +0100

    added function for user inputing move

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //hello
 
-//create function computerPlay
+//create function computerPlay with no parameters
 function computerPlay () {
     //create array "possibleMoves" with rock, paper, scissors
     let possibleMoves = ['rock', 'paper', 'scissors'];
@@ -18,5 +18,23 @@ function getRandNum (max, min) {
     return Math.floor(Math.random() * (max - min) ) + min;
 }
 
+//create function userPlay with no parameters ???
+function userPlay () {
+    //create a variable called userInput that stores input from user
+    let userInput = prompt('What is your move?');
+    //create a variable called lowerCaseInput that converts userInput to lower case
+    let lowerCaseInput = userInput.toLowerCase();
+    //create an If-case that checks if the lowerCaseInput is either rock, paper or scissor
+    if (lowerCaseInput === 'rock' && 'paper' && 'scissor') {
+        //return lowerCaseInput
+        return lowerCaseInput;
+    } else { 
+        //if lowerCaseInput is not a valid choice, print error message to the console
+        console.log('Error! Please enter a valid move.');
+    }
+
+}
+
 //print the returned value from computerPlay
-console.log(computerPlay());
+//console.log(computerPlay());
+console.log(userPlay())
